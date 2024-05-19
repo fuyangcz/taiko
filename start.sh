6fac4e539c93274754fda4356a52014d424b96b1
fuyangcz <101250621+fuyangcz@users.noreply.github.com>
2024-05-20 00:10:53 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,32 +10,6 @@ fi
 # 脚本保存路径
 SCRIPT_PATH="$HOME/Taiko.sh"
 
-
-# 自动设置快捷键的功能
-function check_and_set_alias() {
-    local alias_name="taiko"
-    local shell_rc="$HOME/.bashrc"
-
-    # 对于Zsh用户，使用.zshrc
-    if [ -n "$ZSH_VERSION" ]; then
-        shell_rc="$HOME/.zshrc"
-    elif [ -n "$BASH_VERSION" ]; then
-        shell_rc="$HOME/.bashrc"
-    fi
-
-    # 检查快捷键是否已经设置
-    if ! grep -q "$alias_name" "$shell_rc"; then
-        echo "设置快捷键 '$alias_name' 到 $shell_rc"
-        echo "alias $alias_name='bash $SCRIPT_PATH'" >> "$shell_rc"
-        # 添加提醒用户激活快捷键的信息
-        echo "快捷键 '$alias_name' 已设置。请运行 'source $shell_rc' 来激活快捷键，或重新打开终端。"
-    else
-        # 如果快捷键已经设置，提供一个提示信息
-        echo "快捷键 '$alias_name' 已经设置在 $shell_rc。"
-        echo "如果快捷键不起作用，请尝试运行 'source $shell_rc' 或重新打开终端。"
-    fi
-}
-
 # 节点安装功能
 function install_node() {
 
